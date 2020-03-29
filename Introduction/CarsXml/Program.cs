b9a5c7a609cbcde808dff04420fed48519af1e6a
Maycon Brito <mayconfsbrito@gmail.com>
2020-03-29 14:40:54 -0300

    Linq to XML

---
+++ Introduction/CarsXml/Program.cs
@@ -0,0 +1,95 @@
+﻿using CarXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace CarsXml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateXml();
+            QueryXml();
+        }
+
+        private static void QueryXml()
+        {
+            var ns = (XNamespace)"http://pluralsight.com/cars/2016";
+            var ex = (XNamespace)"http://pluralsight.com/cars/2016/ex";
+            var document = XDocument.Load("fuel.xml");
+
+            var query =
+                from element in document.Element(ns + "Cars")?.Elements(ex + "Car") 
+                    ?? Enumerable.Empty<XElement>()
+                where element.Attribute("Manufacturer")?.Value == "BMW"
+                select element.Attribute("Name").Value;
+
+
+            foreach (var car in query)
+            {
+                Console.WriteLine(car);
+            }
+
+        }
+
+        private static void CreateXml()
+        {
+            var records = ProcessFile("fuel.csv");
+
+            var ns = (XNamespace)"http://pluralsight.com/cars/2016";
+            var ex = (XNamespace)"http://pluralsight.com/cars/2016/ex";
+            var document = new XDocument();
+            var cars = new XElement(ns + "Cars");
+
+            var elements =
+                from record in records
+                select new XElement(
+                    ex + "Car",
+                    new XAttribute("Name", record.Name),
+                    new XAttribute("Combined", record.Combined),
+                    new XAttribute("Manufacturer", record.Manufacturer)
+                );
+
+            cars.Add(new XAttribute(XNamespace.Xmlns + "ex", ex));
+            cars.Add(elements);
+            document.Add(cars);
+            document.Save("fuel.xml");
+        }
+
+        private static IEnumerable<Car> ProcessFile(string path)
+        {
+            return File.ReadAllLines(path)
+                .Skip(1)
+                .Where(line => line.Length > 1)
+                .ToCar();
+            //from line in File.ReadAllLines(path).Skip(1)
+            //where line.Length > 1
+            //select Car.ParseFromCsv(line);
+        }
+    }
+
+    public static class CarExtensions
+    {
+        public static IEnumerable<Car> ToCar(this IEnumerable<string> source)
+        {
+            foreach (var line in source)
+            {
+                var columns = line.Split(',');
+                yield return new Car
+                {
+                    Year = int.Parse(columns[0]),
+                    Manufacturer = columns[1],
+                    Name = columns[2],
+                    Displacement = double.Parse(columns[3]),
+                    Cylinders = int.Parse(columns[4]),
+                    City = int.Parse(columns[5]),
+                    Highway = int.Parse(columns[6]),
+                    Combined = int.Parse(columns[7])
+                };
+            }
+        }
+    }
+}
